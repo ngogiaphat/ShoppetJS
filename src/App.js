1bfeafd45041a0a10f3e8f89d54ef312a584ec3a
ngogiaphat <ngogiaphat0802@gmail.com>
2023-04-08 01:42:29 +0700

    bug cmnr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Greet} from './components/Greet';
+import logo from './logo.svg';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+class App extends Component {
+  render() {
+    return (
+      <div className= "App">
+        <Greet name = "Venti" heroName = "Barbatos">
+          <p>This is children props</p> 
+        </Greet>
+        <Greet name = "Zhongli" heroName = "Morax"/>
+          <button>Action</button>
+        <Greet name = "Raiden Shogun" heroName = "Ei"/>
+        {/* <Greet /> */}
+        <Welcome name = "Venti" heroName = "Barbatos"/>
+        <Welcome name = "Zhongli" heroName = "Morax"/>
+        <Welcome name = "Raiden Shogun" heroName = "Ei"/>
+        {/* <Hello /> */}
+      </div>
+    );
+  }
 }
-
 export default App;
